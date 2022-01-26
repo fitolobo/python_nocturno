a3f4e37446c572838759b4e642cd7e8f933c7834
fitolobo <37717782+fitolobo@users.noreply.github.com>
2022-01-26 01:55:02 -0300

    Add files via upload

---
+++ PYTHON/intro.py
@@ -0,0 +1,26 @@
+# Sumando
+
+x = 1
+y = 2
+
+print("La suma de x e y: ", x+y)
+print("########################")
+print()
+
+# Restar
+z = 5
+w = 6
+
+d = w - z
+
+
+print(w - z)
+
+# Suma final
+suma_total = x+y+w+z
+
+
+
+
+# Comenté
+#print("Hola cauros")
