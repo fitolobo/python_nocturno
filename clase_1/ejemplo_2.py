c0157dd11c6f61327411e8fb763849ac00d57d9a
fitolobo <37717782+fitolobo@users.noreply.github.com>
2022-02-01 09:14:40 -0300

    Add files via upload

---
+++ clase_1/ejemplo_2.py
@@ -0,0 +1,16 @@
+
+
+# Para instalar usamos pip
+
+# Asi se hace en el terminal:
+# pip install nombre_libreria
+
+# En el notebook se hace asi:
+# !pip install nombre_libreria
+
+
+# para saber que version instale (en el terminal)
+# pip freeze
+
+# stackoverflow es el mejor foro
+# siempre buscar documentacion
